633e19afb51715e9db0c5849940145f94d55498f
Koetemagie <koetemagie@gmail.com>
2021-02-18 01:09:43 -0300

    featured snippet fix & translation parser
    changed translation parser and fixed 'a' problem in featured snippet?

--- tuxiqualitytest.sh
+++ tuxiqualitytest.sh
@@ -1,10 +1,14 @@
-echo "Testing Translation¬" && tuxi "Vais para cascais? em ingles" || echo "\tFailed..."
-echo "\nTesting Math¬" && tuxi "log(30)" && tuxi "(40/3)+4*6" || echo "\tFailed..."
-echo "\nTesting Lyrics¬" && tuxi "the motans inainte sa ne fi nascut lyrics" || echo "\tFailed..."
-echo "\nTesting Featured¬" && tuxi "the meaning of life the universe and everything else" || echo "\tFailed..."
-echo "\nTesting Rich Answer¬" && tuxi "elevation of mt everest" || echo "\tFailed..."
-echo "\nTesting kno_rigth¬" && tuxi "lorem ipsum" || echo "\tFailed..."
-echo "\nTesting kno_top¬" && tuxi "the office cast" || echo "\tFailed..."
-echo "\nTesting Error corrrection¬" && tuxi "Linux Tarvalds" || echo "\tFailed..."
-echo "\nTesting help message¬" && tuxi -h || echo "\tFailed..."
+errmsg="\tFailed..."
+
+echo "Testing Translation¬" && tuxi "Vais para cascais? em ingles" || echo $errmsg
+echo "\nTesting Math¬" && tuxi "log(30)" && tuxi "(40/3)+4*6" || echo $errmsg
+echo "\nTesting basic_top¬" && tuxi "christmas day" || echo $errmsg
+echo "\nTesting Lyrics¬" && tuxi "the motans inainte sa ne fi nascut lyrics" || echo $errmsg
+echo "\nTesting Featured¬" && tuxi "the meaning of life the universe and everything else" || echo $errmsg
+echo "\nTesting Rich Answer¬" && tuxi "elevation of mt everest" || echo $errmsg
+echo "\nTesting kno_rigth¬" && tuxi "lorem ipsum" || echo $errmsg
+echo "\nTesting kno_top¬" && tuxi "the office cast" || echo $errmsg
+echo "\nTesting weather¬" && tuxi "weather new york" || echo $errmsg
+echo "\nTesting Error corrrection¬" && tuxi "Linux Tarvalds" || echo $errmsg
+echo "\nTesting help message¬" && tuxi -h || echo $errmsg
 
