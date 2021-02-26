5d1a8a93e8594b776efe7dcb0513989620f3873b
oscar <oscarlay789@gmail.com>
2021-02-25 21:25:23 -0600

    Made test script prettier

--- tuxiqualitytest.sh
+++ tuxiqualitytest.sh
@@ -16,15 +16,19 @@ while getopts "r" o; do
         exit 1
         ;;
     esac
-done
-#this took to long for me to figure out
-
+done #this took to long for me to figure out 
 if $raw; then
     red="" 
     run="-r"
 fi
 
+#Clean up the code function
+test_code () {
+    # echo ${1}  "\"${2}\"" \n \n
+    # printf "${1}¬\n" && ./tuxi ${run} \""${2}"\" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
+    printf "${1}¬\n" && ./tuxi ${run} ${2} 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
 
+}
 
 printf "${red}Test Output - If you don't know what you're doing here, hit h to figure out controls, or q if you aren't developing\n" >> testoutputs.txt
 printf "${red}###################################################################################################################\n" >> testoutputs.txt
@@ -35,81 +39,85 @@ if ! $raw; then
     printf "Testing help message¬\n" && ./tuxi -h 1>>testoutputs.txt && printf "\tpassed.\n"|| printf "\tFailed...\n"
 fi
 #Help message cant be sent as raw, tuxi -r -h does not work
-
-printf "Testing error corrrection¬\n" && ./tuxi $run "Linux Tarvalds" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing error corrrection¬\n" && ./tuxi $run "waht is the office" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing error corrrection¬\n" && ./tuxi $run "hwo much is btc wroth" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-
-
-
-printf "Testing Math¬\n" && ./tuxi $run "log(30)" 1>>testoutputs.txt && ./tuxi $run "(40/3)+4*6" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Math¬\n" && ./tuxi $run "72^43/12(44+1)-3124" 1>>testoutputs.txt && ./tuxi $run "45/sqrt(4)*3" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-
-
-printf "Testing Knowledge Graph - top¬\n" && ./tuxi $run "the office cast" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Knowledge Graph - top¬\n" && ./tuxi $run "legends of runeterra" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Knowledge Graph - top¬\n" && ./tuxi $run "reddit" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Knowledge Graph - top¬\n" && ./tuxi $run "twitter" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-
-
-printf "Testing Rich Answer¬\n" && ./tuxi $run "elevation of mt everest" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Rich Answer¬\n" && ./tuxi $run "what is elevation of mt rainier" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Rich Answer¬\n" && ./tuxi $run "how big is the grand canyon" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Rich Answer¬\n" && ./tuxi $run "how long is the yellow river?" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
+test_code "Testing error corrrectionn" "Linux Tarvalds"
+test_code "Testing error corrrectionn" "waht is the office" 
+test_code "Testing error corrrectionn" "hwo much is btc wroth" 
 
 
 
-printf "Testing Featured Snippets¬\n" && ./tuxi $run "the meaning of life the universe and everything else" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Featured Snippets¬\n" && ./tuxi $run "shotgun doom eternal" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Featured Snippets¬\n" && ./tuxi $run "what is tenacity in league of legends" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Featured Snippets¬\n" && ./tuxi $run "rusty cage bio" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
+test_code "Testing Math" "log(30)" 
+test_code "Testing Math" "72^43/12(44+1)-3124"
+test_code "Testing Math" "45/sqrt(4)*3"
+
+
+test_code "Testing Knowledge Graph - top" "the office cast" 
+test_code "Testing Knowledge Graph - top" "hamilton cast" 
+test_code "Testing Knowledge Graph - top" "legends of runeterra"
+test_code "Testing Knowledge Graph - top" "reddit" 
+test_code "Testing Knowledge Graph - top" "twitter" 
+
+
+test_code "Testing Rich Answer" "elevation of mt everest" 
+test_code "Testing Rich Answer" "what is elevation of mt rainier" 
+test_code "Testing Rich Answer" "how big is the grand canyon" 
+test_code "Testing Rich Answer" "how long is the yellow river?" 
 
 
 
-printf "Testing Lyrics¬\n" && ./tuxi $run "the motans inainte sa ne fi nascut lyrics" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Lyrics¬\n" && ./tuxi $run "lyrics of hello adele" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Lyrics¬\n" && ./tuxi $run "Toosie slide lyrics" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Lyrics¬\n" && ./tuxi $run "Sigues con el lyrics" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Lyrics¬\n" && ./tuxi $run "my truck breland lyrics" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Lyrics¬\n" && ./tuxi $run "maniac conan gray lyrics" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Lyrics¬\n" && ./tuxi $run "dealer RMR lyrics" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
+test_code "Testing Featured Snippets" "the meaning of life the universe and everything else" 
+test_code "Testing Featured Snippets" "shotgun doom eternal" 
+test_code "Testing Featured Snippets" "what is tenacity in league of legends" 
+test_code "Testing Featured Snippets" "rusty cage bio" 
 
 
-printf "Testing Units¬\n" && ./tuxi $run "100 cm to m" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Units¬\n" && ./tuxi $run "100 c to f" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Units¬\n" && ./tuxi $run "convert 100 c to f" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Units¬\n" && ./tuxi $run "what is 10 feet to meters" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
+
+test_code "Testing Lyrics" "the motans inainte sa ne fi nascut lyrics" 
+test_code "Testing Lyrics" "lyrics of hello adele" 
+test_code "Testing Lyrics" "Toosie slide lyrics" 
+test_code "Testing Lyrics" "Sigues con el lyrics" 
+test_code "Testing Lyrics" "my truck breland lyrics" 
+test_code "Testing Lyrics" "maniac conan gray lyrics" 
+test_code "Testing Lyrics" "dealer RMR lyrics" 
 
 
-printf "Testing Currency¬\n" && ./tuxi $run "100 GBP to USD" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Currency¬\n" && ./tuxi $run "what is 10000 GBP in USD" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Currency¬\n" && ./tuxi $run "how much is 1 btc in USD" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
+test_code "Testing Units" "100 cm to m" 
+test_code "Testing Units" "100 c to f" 
+test_code "Testing Units" "convert 100 c to f" 
+test_code "Testing Units" "what is 10 feet to meters" 
 
 
-printf "Testing Translate¬\n" && ./tuxi $run "Vais para cascais? em ingles" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Translate¬\n" && ./tuxi $run "برمجة in english" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Translate¬\n" && ./tuxi $run "mañana in english?" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Translate¬\n" && ./tuxi $run "what is mananeo in english" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-#it looks like you can only convert to english without error 
+test_code "Testing Currency" "100 GBP to USD" 
+test_code "Testing Currency" "what is 10000 GBP in USD" 
+test_code "Testing Currency" "how much is 1 btc in USD" 
 
 
-printf "Testing Weather¬\n" && ./tuxi $run "weather new york" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Weather¬\n" && ./tuxi $run "how hot is it in india?" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Weather¬\n" && ./tuxi $run "how cold is it in alaska?" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Weather¬\n" && ./tuxi $run "temp of mexico right now?" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Weather¬\n" && ./tuxi $run "how cold is it in canada right now?" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
+test_code "Testing Translate" "Vais para cascais? em ingles" 
+test_code "Testing Translate" "برمجة in english" 
+test_code "Testing Translate" "mañana in english?" 
+test_code "Testing Translate" "what is mananeo in english" 
+#looks like you can only convert to english without error 
 
 
-printf "Testing Knowledge Graph - right¬\n" && ./tuxi $run "lorem ipsum" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Knowledge Graph - right¬\n" && ./tuxi $run "the office" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Knowledge Graph - right¬\n" && ./tuxi $run "what is a firefighter" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing Knowledge Graph - right¬\n" && ./tuxi $run "what is the american bill of rights" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
+test_code "Testing Weather" "weather new york" 
+test_code "Testing Weather" "how hot is it in india?" 
+test_code "Testing Weather" "how cold is it in alaska?" 
+test_code "Testing Weather" "temp of mexico right now?" 
+test_code "Testing Weather" "how cold is it in canada right now?" 
 
 
-printf "Testing How to Pronounce¬\n" && ./tuxi $run "how to pronounce supercalifragilisticexpialidocious" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing How to Pronounce¬\n" && ./tuxi $run "pronunciation of worcestershire" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
-printf "Testing How to Pronounce¬\n" && ./tuxi $run "pronounce almond" 1>>testoutputs.txt && printf "\tpassed.\n" || printf "\tFailed...\n"
+test_code "Testing Knowledge Graph - right" "lorem ipsum" 
+test_code "Testing Knowledge Graph - right" "the office" 
+test_code "Testing Knowledge Graph - right" "what is a firefighter" 
+test_code "Testing Knowledge Graph - right" "what is the american bill of rights" 
+
+
+test_code "Testing How to Pronounce" "how to pronounce supercalifragilisticexpialidocious" 
+test_code "Testing How to Pronounce" "pronunciation of worcestershire" 
+test_code "Testing How to Pronounce" "pronounce almond" 
 #THIS DOES NOT WORK FOR SOME WORDS ON MY END, i.e. gnocchi, charcuterie
+
+
+
 ########################################
 #not yet implemented in dev2
 ########################################
